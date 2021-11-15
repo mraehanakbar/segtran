dae8c3c5a39af9014ff7af44cd0f7a54a03a8c87
mraehanakbar <43035458+mraehanakbar@users.noreply.github.com>
2021-11-15 17:43:53 +0700

    Update __init__.py

--- code/MNet_DeepCDR/mnet_deep_cdr/__init__.py
+++ code/MNet_DeepCDR/mnet_deep_cdr/__init__.py
@@ -12,7 +12,7 @@ __version__ = '0.0.2'
 
 
 def get_logger(name=None):
-    with open(path.join(path.dirname(__file__), '/content/segtran/code/MNet_DeepCDR/mnet_deep_cdr/_data/logging.yml', 'logging.yml'), 'rt') as f:
+    with open(path.join(path.dirname(__file__), '/content/segtran/code/MNet_DeepCDR/mnet_deep_cdr/_data', 'logging.yml'), 'rt') as f:
         data = yaml.load(f)
     _dictConfig(data)
     return logging.getLogger(name=name)
